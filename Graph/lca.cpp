d79b211491c73f97a383e03ab344d885920ebc73
Rafael Acosta <theGitNoob@gmail.com>
2023-10-31 13:09:39 -0400

    Improved lca

--- Graph/lca.cpp
+++ Graph/lca.cpp
@@ -8,121 +8,98 @@
 
 using namespace std;
 
-// begin
 class SparseTable
 {
-
 private:
-    vector<vi> lookup;
-
-    vi arr;
-
-    int rmq(int a, int b)
-    {
-        if (arr[a] <= arr[b])
-            return a;
-
-        return b;
-    }
-
-    int operation(int a, int b)
-    {
-        return rmq(a, b);
-    }
-
-    void build_sparse_table()
-    {
-        int n = arr.size();
-
-        for (int i = 0; i < n; i++)
-            lookup[i][0] = i;
-
-        for (int j = 1; (1 << j) <= n; j++)
-        {
-            for (int i = 0; i <= n - (1 << j); i++)
-                lookup[i][j] = operation(lookup[i][j - 1], lookup[i + (1 << (j - 1))][j - 1]);
-        }
-    }
+    vi arr, p2, l2;
+    vector<vi> spt;
 
 public:
-    SparseTable(vi &a)
+    SparseTable(int n, int *A)
     {
-        int q = (int)log2(a.size());
-
-        arr.assign(a.size(), 0);
-        lookup.assign(a.size(), vi(q + 1));
-
-        for (int i = 0; i < a.size(); i++)
-            arr[i] = a[i];
-
-        build_sparse_table();
+        arr.assign(A + 1, A + n + 1);
+        build_spt();
     }
-
-    int query(int l, int r)
+    SparseTable(vi A)
     {
-        int q = (int)log2(r - l + 1);
-
-        return operation(lookup[l][q],
-                         lookup[r - (1 << q) + 1][q]);
+        arr = A;
+        build_spt();
     }
-
-    int get(int i) { return arr[i]; }
-};
-
-void dfs(int n, int p, int d, vi adj[], vi &deep, vi &arr)
-{
-    deep[n] = d;
-    arr.push_back(n);
-
-    for (int i = 0; i < adj[n].size(); i++)
+    void build_spt()
     {
-        if (p != adj[n][i])
+        int n = arr.size();
+        int L2_n = (int)log2(n) + 1;
+        p2.assign(L2_n + 1, 0);
+        l2.assign((1 << L2_n) + 1, 0);
+
+        for (int i = 0; i <= L2_n; ++i)
         {
-            dfs(adj[n][i], n, d + 1, adj, deep, arr);
-            arr.push_back(n);
+            p2[i] = (1 << i);
+            l2[(1 << i)] = i;
         }
+        for (int i = 2; i < p2[L2_n]; ++i)
+            if (l2[i] == 0)
+                l2[i] = l2[i - 1];
+
+        spt = vector<vi>(l2[n] + 1, vi(n));
+
+        for (int j = 0; j < n; ++j)
+            spt[0][j] = j;
+
+        for (int i = 1; p2[i] <= n; ++i)
+            for (int j = 0; j + p2[i] - 1 < n; ++j)
+            {
+                int x = spt[i - 1][j];
+                int y = spt[i - 1][j + p2[i - 1]];
+                spt[i][j] = arr[x] <= arr[y] ? x : y;
+            }
+    }
+    int rmq(int i, int j)
+    {
+        if (i > j)
+            swap(i, j);
+        int k = l2[j - i + 1];
+        int x = spt[k][i];
+        int y = spt[k][j - p2[k] + 1];
+        return arr[x] <= arr[y] ? x : y;
+    }
+};
+int first_ocurr[MAXN], euler_tour[2 * MAXN], depth[MAXN * 2], D[MAXN], idx;
+void lca_dfs(int node, int d, int parent = -1)
+{
+    euler_tour[++idx] = node;
+    depth[idx] = d;
+    first_ocurr[node] = idx;
+    D[node] = d;
+
+    for (auto &nxt : ady[node])
+    {
+        if (nxt == parent)
+            continue;
+        lca_dfs(nxt, d + 1, node);
+        euler_tour[++idx] = node;
+        depth[idx] = d;
     }
 }
-
-vi lca(int root, int n, vi adj[], vii &querys)
+SparseTable *spt;
+void build_lca()
 {
-    vi deep(n);
-    vi first(n);
-    vi last(n);
-    vi arr;
-    vi resp(querys.size());
+    idx = 0;
+    lca_dfs(1, 0);
+    spt = new SparseTable(idx, depth);
+}
+int lca_query(int a, int b)
+{
+    if (depth[a] > depth[b])
+        swap(a, b);
 
-    dfs(root, -1, 0, adj, deep, arr);
+    return euler_tour[spt->rmq(first_ocurr[a] - 1, first_ocurr[b] - 1) + 1];
+}
+int dist(int a, int b)
+{
+    int lca = lca_query(a, b);
 
-    for (int i = 0; i < arr.size(); i++)
-        last[arr[i]] = i;
-
-    for (int i = arr.size() - 1; i >= 0; i--)
-        first[arr[i]] = i;
-
-    vi arr_deep(arr.size());
-
-    for (int i = 0; i < arr_deep.size(); i++)
-        arr_deep[i] = deep[arr[i]];
-
-    auto s = SparseTable(arr_deep);
-
-    for (int i = 0; i < querys.size(); i++)
-    {
-        int l = first[querys[i].first];
-        int r = last[querys[i].second];
-
-        if (l > r)
-        {
-            r = last[querys[i].first];
-            l = first[querys[i].second];
-        }
-
-        int q = s.query(l, r);
-        resp[i] = arr[q];
-    }
-
-    return resp;
+    return D[a] + D[b] - 2 * D[lca];
 }
 // end
 
