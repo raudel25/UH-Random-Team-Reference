21436269f813fe4143d4aab3b218198d5552d17d
Rafael Acosta <theGitNoob@gmail.com>
2024-07-18 15:34:04 -0400

    feat: improve DSU

--- DataStructure/disjoint_set_union.cpp
+++ DataStructure/disjoint_set_union.cpp
@@ -1,29 +1,41 @@
 #include <bits/stdc++.h>
- 
+
 using namespace std;
 typedef vector<int> vi;
 
 // begin
- 
-struct dsu {
-    vi p;
-    void init(int n) {
-        p = vi(n, -1);
+
+struct dsu
+{
+    vi p, rank, sz;
+    int num_sets;
+    dsu(int n)
+    {
+        p.assign(n + 1, 0);
+        iota(p.begin(), p.end(), 0);
+        rank.assign(n + 1, 0);
+        sz.assign(n + 1, 1);
+        num_sets = n;
     }
-    int get(int x) {
+    int get(int x)
+    {
         if (p[x] < 0)
             return x;
         return p[x] = get(p[x]);
     }
-    void unite(int a, int b) {
+    bool is_same_set(int a, int b) { return get(a) == get(b); }
+    int size_of(int a) { return sz[get(a)]; }
+    void unite(int a, int b)
+    {
         a = get(a);
         b = get(b);
-        if (a != b) {
-            if (p[a] > p[b])
-                swap(a, b);
-            p[a] += p[b];
-            p[b] = a;
-        }
+        if (rank[a] > rank[b])
+            swap(a, b);
+        p[a] = b;
+        if (rank[a] == rank[b])
+            rank[b]++;
+        sz[b] += sz[a];
+        num_sets--;
     }
 };
 
