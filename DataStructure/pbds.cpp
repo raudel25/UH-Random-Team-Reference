eee629de3a44f7c87552262d6374fdd370a5fd0b
Rafael Acosta <theGitNoob@gmail.com>
2024-10-25 22:39:23 -0400

    fix: change include header

--- DataStructure/pbds.cpp
+++ DataStructure/pbds.cpp
@@ -3,8 +3,6 @@ using namespace std;
 
 // begin
 #include <ext/pb_ds/assoc_container.hpp>
-#include <ext/pb_ds/tree_policy.hpp>
-#include <bits/extc++.h> // pbds
 using namespace __gnu_pbds;
 typedef tree<int, null_type, less<int>, rb_tree_tag,
              tree_order_statistics_node_update>
