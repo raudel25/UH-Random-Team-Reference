2dd00225549b8ed1eee9a50d4df353f1e6ac0139
Rafael Acosta <theGitNoob@gmail.com>
2024-11-02 22:32:12 -0400

    fix: change wrong hashing implementation

--- String/hashing.cpp
+++ String/hashing.cpp
@@ -20,58 +20,65 @@ typedef vector<ll> vll;
 typedef vector<pii> vii;
 
 // begin
-struct custom_hash
-{
-    static uint64_t splitmix64(uint64_t x)
-    {
-        x += 0x9e3779b97f4a7c15;
-        x = (x ^ (x >> 30)) * 0xbf58476d1ce4e5b9;
-        x = (x ^ (x >> 27)) * 0x94d049bb133111eb;
-        return x ^ (x >> 31);
-    }
+typedef long long ll;
+const int MAXN = 1e5 + 1;
 
-    size_t operator()(uint64_t x) const
-    {
-        static const uint64_t FIXED_RANDOM =
-            chrono::steady_clock::now().time_since_epoch().count();
-        return splitmix64(x + FIXED_RANDOM);
-    }
-};
+const int p = 131;     // p and M are
+const int M = 1e9 + 7; // relatively prime
 
-const int MAXN = 5e5 + 5;
-const int MOD = 1e9 + 7;
-int mod_pow(int b, int e)
-{
-    int res = 1;
-    while (e)
-    {
-        if (e & 1)
-            res = res * b % MOD;
-        b = b * b % MOD;
-        e >>= 1;
+int Pow[MAXN]; // to store p^i % M
+int h[MAXN];   // to store prefix hashes
+int n;
+char T[MAXN];
+int P[MAXN];
+
+void computeRollingHash()
+{                               // Overall: O(n)
+    Pow[0] = 1;                 // compute p^i % M
+    for (int i = 1; i < n; ++i) // O(n)
+        Pow[i] = ((ll)Pow[i - 1] * p) % M;
+    h[0] = 0;
+    for (int i = 0; i < n; ++i)
+    { // O(n)
+        if (i != 0)
+            h[i] = h[i - 1]; // rolling hash
+        h[i] = (h[i] + ((ll)T[i] * Pow[i]) % M) % M;
     }
-    return res % MOD;
 }
 
-typedef unordered_map<ll, ll, custom_hash> safe_map;
-
-int dx[] = {1, 0, -1, 0, 1, -1, -1, 1};
-int dy[] = {0, 1, 0, -1, 1, 1, -1, -1};
-
-int dp[MAXN];
-vi ady_list[MAXN];
-
-int n, m, t, k;
-int f[MAXN];
-int comb(int n, int k)
-{
-    int fk = mod_pow(f[k], MOD - 2);
-    int fnk = mod_pow(f[n - k], MOD - 2);
-    int x = ((f[n] * fk) % MOD * fnk % MOD);
-
-    return x;
+int extEuclid(int a, int b, int &x, int &y)
+{ // pass x and y by ref
+    int xx = y = 0;
+    int yy = x = 1;
+    while (b)
+    { // repeats until b == 0
+        int q = a / b;
+        tie(a, b) = tuple(b, a % b);
+        tie(x, xx) = tuple(xx, x - q * xx);
+        tie(y, yy) = tuple(yy, y - q * yy);
+    }
+    return a; // returns gcd(a, b)
 }
 
+int modInverse(int b, int m)
+{ // returns b^(-1) (mod m)
+    int x, y;
+    int d = extEuclid(b, m, x, y); // to get b*x + m*y == d
+    if (d != 1)
+        return -1; // to indicate failure
+    // b*x + m*y == 1, now apply (mod m) to get b*x == 1 (mod m)
+    return (x + m) % m; // this is the answer
+}
+
+int hash_fast(int l, int r)
+{ // O(1) hash of any substr
+    if (l == 0)
+        return h[r]; // h is the prefix hashes
+    int ans = 0;
+    ans = ((h[r] - h[l - 1]) % M + M) % M;       // compute differences
+    ans = ((ll)ans * modInverse(Pow[l], M)) % M; // remove P[L]^-1 (mod M)
+    return ans;
+}
 // end
 
 int32_t main()
@@ -79,22 +86,5 @@ int32_t main()
 
     ios_base::sync_with_stdio(0);
     cin.tie(0);
-    f[0] = f[1] = 1;
-    forl(i, 2, MAXN - 1) f[i] = f[i - 1] * i % MOD;
-    cin >> n;
-    dp[1] = 1;
-    forl(i, 2, n)
-    {
-        forl(j, 1, i - 1)
-        {
-            int s = comb(i, j);
-            int z = ((s * dp[j] % MOD) * dp[i - j]);
-            // if (j == i - j)
-            // z = z * mod_pow(2, MOD - 2);
-            dp[i] += z % MOD;
-            dp[i] %= MOD;
-        }
-    }
-    cout << dp[n];
     return 0;
 }
