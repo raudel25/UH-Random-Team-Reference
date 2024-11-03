80d11a74700865f07b128ce707d47e893e217e0d
Rafael Acosta <theGitNoob@gmail.com>
2024-11-02 22:30:24 -0400

    refactor: improve suffix array implementation

--- String/suffix_array.cpp
+++ String/suffix_array.cpp
@@ -2,7 +2,7 @@
 
 #define MAX(a, b) (a > b) ? a : b
 #define MIN(a, b) (a < b) ? a : b
-#define int long long
+// #define int long long
 #define vi vector<int>
 #define pii pair<int, int>
 #define vii vector<pii>
@@ -13,190 +13,146 @@ using namespace std;
 class SuffixArray
 {
 
-public:
-    SuffixArray(string s)
-    {
-        n = s.size() + 1;
-        s_value = s + "$";
-
-        ra.assign(n, 0);
-        sa.assign(n, 0);
-        temp_ra.assign(n, 0);
-        temp_sa.assign(n, 0);
-
-        construct_sa();
-        build_lcp();
-    }
-
-    int size() { return n; }
-
-    int get_int(int i) { return sa[i]; }
-
-    int cant_match(string p)
-    {
-        pii ans = matching(p);
-
-        if (ans.first == -1 && ans.second == -1)
-            return 0;
-
-        return ans.second - ans.first + 1;
-    }
-
-    int get_lcp(int i) { return plcp[sa[i]]; }
-
-    int cant_substr() { return v_cant_substr; }
-
-    string get_str(int i) { return s_value.substr(sa[i], n - sa[i] - 1); }
-
-private:
-    string s_value;
-    int n;
-    int v_cant_substr;
-
-    vi ra;
-    vi sa;
-    vi c;
-    vi temp_ra;
-    vi temp_sa;
-    vi phi;
-    vi plcp;
-
-    void counting_sort(int k)
-    {
-        int sum = 0;
-        int maxi = max((int)300, n);
-
-        c.assign(maxi, 0);
-
-        for (int i = 0; i < n; i++)
-            c[i + k < n ? ra[i + k] : 0]++;
-
-        for (int i = 0; i < maxi; i++)
+    void countingSort(int k)
+    {                                       // O(n)
+        int maxi = max(300, n);             // up to 255 ASCII chars
+        vi c(maxi, 0);                      // clear frequency table
+        for (int i = 0; i < n; ++i)         // count the frequency
+            ++c[i + k < n ? RA[i + k] : 0]; // of each integer rank
+        for (int i = 0, sum = 0; i < maxi; ++i)
         {
-            int tx = c[i];
+            int t = c[i];
             c[i] = sum;
-            sum += tx;
+            sum += t;
         }
-
-        for (int i = 0; i < n; i++)
-            temp_sa[c[sa[i] + k < n ? ra[sa[i] + k] : 0]++] = sa[i];
-
-        for (int i = 0; i < n; i++)
-            sa[i] = temp_sa[i];
+        vi tempSA(n);
+        for (int i = 0; i < n; ++i) // sort SA
+            tempSA[c[SA[i] + k < n ? RA[SA[i] + k] : 0]++] = SA[i];
+        swap(SA, tempSA); // update SA
     }
 
-    void construct_sa()
-    {
-        int k, r;
-
-        for (int i = 0; i < n; i++)
-        {
-            ra[i] = s_value[i];
-            sa[i] = i;
-        }
-
-        for (k = 1; k < n; k <<= 1)
-        {
-            counting_sort(k);
-            counting_sort(0);
-
-            temp_ra[sa[0]] = r = 0;
-
-            for (int i = 1; i < n; i++)
-                temp_ra[sa[i]] = (ra[sa[i]] == 
-                    ra[sa[i - 1]] && ra[sa[i] + k] == ra[sa[i - 1] + k]) ? r : ++r;
-
-            for (int i = 0; i < n; i++)
-                ra[i] = temp_ra[i];
-
-            if (ra[sa[n - 1]] == n - 1)
-                break;
+    void constructSA()
+    { // can go up to 400K chars
+        SA.resize(n);
+        iota(SA.begin(), SA.end(), 0); // the initial SA
+        RA.resize(n);
+        for (int i = 0; i < n; ++i)
+            RA[i] = T[i]; // initial rankings
+        for (int k = 1; k < n; k <<= 1)
+        { // repeat log_2 n times
+            // this is actually radix sort
+            countingSort(k); // sort by 2nd item
+            countingSort(0); // stable-sort by 1st item
+            vi tempRA(n);
+            int r = 0;
+            tempRA[SA[0]] = r;          // re-ranking process
+            for (int i = 1; i < n; ++i) // compare adj suffixes
+                tempRA[SA[i]] =         // same pair => same rank r; otherwise, increase r
+                    ((RA[SA[i]] == RA[SA[i - 1]]) && (RA[SA[i] + k] == RA[SA[i - 1] + k])) ? r : ++r;
+            swap(RA, tempRA); // update RA
+            if (RA[SA[n - 1]] == n - 1)
+                break; // nice optimization
         }
     }
 
-    pii matching(string p)
+    void computeLCP()
     {
-        int l = 0;
-        int r = n - 1;
-        int p_size = p.size();
-
-        string comp;
-
-        while (l < r)
-        {
-            int m = (l + r) / 2;
-
-            comp = s_value.substr(sa[m], min(n - sa[m], p_size));
-
-            if (comp >= p)
-                r = m;
-            else
-                l = m + 1;
-        }
-
-        comp = s_value.substr(sa[l], min(n - sa[l], p_size));
-
-        if (comp != p)
-            return {-1, -1};
-
-        int ans_l = l;
-
-        l = 0;
-        r = n - 1;
-
-        while (l < r)
-        {
-            int m = (l + r) / 2;
-
-            comp = s_value.substr(sa[m], min(n - sa[m], p_size));
-
-            if (comp > p)
-                r = m;
-            else
-                l = m + 1;
-        }
-
-        comp = s_value.substr(sa[r], min(n - sa[r], p_size));
-
-        if (comp != p)
-            r--;
-
-        int ans_r = r;
-
-        return {ans_l, ans_r};
-    }
-
-    void build_lcp()
-    {
-        phi.assign(n, 0);
-        plcp.assign(n, 0);
-
-        phi[0] = -1;
-
-        for (int i = 1; i < n; i++)
-            phi[sa[i]] = sa[i - 1];
-
-        int l = 0;
-        int q = 0;
-        for (int i = 0; i < n; i++)
-        {
-            if (phi[i] == -1)
+        vi Phi(n);
+        vi PLCP(n);
+        PLCP.resize(n);
+        Phi[SA[0]] = -1;            // default value
+        for (int i = 1; i < n; ++i) // compute Phi in O(n)
+            Phi[SA[i]] = SA[i - 1]; // remember prev suffix
+        for (int i = 0, L = 0; i < n; ++i)
+        { // compute PLCP in O(n)
+            if (Phi[i] == -1)
             {
-                plcp[i] = 0;
+                PLCP[i] = 0;
                 continue;
-            }
-
-            while (s_value[i + l] == s_value[phi[i] + l])
-                l++;
-
-            plcp[i] = l;
-            q += l;
-            l = max(l - 1, (int)0);
+            } // special case
+            while ((i + L < n) && (Phi[i] + L < n) && (T[i + L] == T[Phi[i] + L]))
+                ++L; // L incr max n times
+            PLCP[i] = L;
+            L = max(L - 1, 0); // L dec max n times
         }
+        LCP.resize(n);
+        for (int i = 0; i < n; ++i) // compute LCP in O(n)
+            LCP[i] = PLCP[SA[i]];   // restore PLCP
+    }
 
-        v_cant_substr = n * (n - 1) / 2 - q;
+public:
+    const char *T; // the input string
+    const int n;   // the length of T
+    vi SA;         // Suffix Array
+    vi LCP;        // of adj sorted suffixes
+    vi RA;         // rank array
+
+    SuffixArray(const char *initialT, const int _n) : T(initialT), n(_n)
+    {
+        constructSA(); // O(n log n)
+        computeLCP();  // O(n)
+    }
+    SuffixArray(const string &s) : T(s.c_str()), n(s.size())
+    {
+        constructSA();
+        computeLCP();
+    }
+    pii stringMatching(const string &P)
+    {
+        return stringMatching(P.c_str());
+    }
+
+    pii stringMatching(const char *P)
+    {                           // in O(m log n)
+        int m = (int)strlen(P); // usually, m < n
+        int lo = 0, hi = n - 1; // range = [0..n-1]
+        while (lo < hi)
+        {                                         // find lower bound
+            int mid = (lo + hi) / 2;              // this is round down
+            int res = strncmp(T + SA[mid], P, m); // P in suffix SA[mid]?
+            (res >= 0) ? hi = mid : lo = mid + 1; // notice the >= sign
+        }
+        if (strncmp(T + SA[lo], P, m) != 0)
+            return {-1, -1}; // if not found
+        pii ans;
+        ans.first = lo;
+        hi = n - 1; // range = [lo..n-1]
+        while (lo < hi)
+        { // now find upper bound
+            int mid = (lo + hi) / 2;
+            int res = strncmp(T + SA[mid], P, m);
+            (res > 0) ? hi = mid : lo = mid + 1; // notice the > sign
+        }
+        if (strncmp(T + SA[hi], P, m) != 0)
+            --hi; // special case
+        ans.second = hi;
+        return ans; // returns (lb, ub)
+    } // where P is found
+
+    pii LRS()
+    { // (LRS length, index)
+        int idx = 0, maxLCP = -1;
+        for (int i = 1; i < n; ++i) // O(n), start from i = 1
+            if (LCP[i] > maxLCP)
+                maxLCP = LCP[i], idx = i;
+        return {maxLCP, idx};
+    }
+
+    pii LCS(int split_idx)
+    { // (LCS length, index)
+        int idx = 0, maxLCP = -1;
+        for (int i = 1; i < n; ++i)
+        { // O(n), start from i = 1
+            // if suffix SA[i] and suffix SA[i-1] came from the same string, skip
+            if ((SA[i] < split_idx) == (SA[i - 1] < split_idx))
+                continue;
+            if (LCP[i] > maxLCP)
+                maxLCP = LCP[i], idx = i;
+        }
+        return {maxLCP, idx};
     }
 };
+
 // end;
 
 void solve()
@@ -207,7 +163,7 @@ void solve()
     SuffixArray suffix(s);
     int n = s.size();
 
-    cout << suffix.cant_substr() << "\n";
+    // cout << suffix.cant_substr() << "\n";
 }
 
 int32_t main()
